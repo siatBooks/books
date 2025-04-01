b3d3777526c76e721c69b4c840caf9686f844575
eycho93 <eycho93.eyc@gmail.com>
2025-04-02 00:51:31 +0900

    ey test

---
+++ todo/domain/categoryRequestDTO.java
@@ -0,0 +1,7 @@
+public class categoryRequestDTO {
+    private int     category_id ; 
+    private String  name;
+    private String  publisher;
+    private int     bestrank;
+    private int     parent_category_id;
+}
