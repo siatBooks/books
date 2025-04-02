273cae52931e180f189b5306e31dfd3d3a59d2a0
kth <rnjsxo10174@gmail.com>
2025-04-02 10:19:53 +0900

    [th] 기본 설계 예시

---
+++ src/controller/FrontController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import factory.BeanFactory;
+
+public class FrontController {
+    private BeanFactory factory;
+
+    public FrontController() {
+        factory = BeanFactory.getInstance();
+    }
+}
