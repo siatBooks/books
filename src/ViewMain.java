60a6e9634d21d247835acaa0323522d1515b49da
davJ-star <jshsy0130@gmail.com>
2025-04-02 16:18:20 +0900

    Enhance user interaction in ViewMain.java with additional options and improved output formatting

--- src/ViewMain.java
+++ src/ViewMain.java
@@ -62,6 +62,7 @@ public class ViewMain {
         System.out.println("시스템 종료? (y/n)");
         Scanner scanner1 = new Scanner(System.in);
         String str = scanner1.nextLine();
+
         if (str.equals("y")) {
             System.exit(0);
         } else if (str.equals("n")) {
@@ -104,6 +105,9 @@ public class ViewMain {
 
 
 
+    private static void home() {
+        
+    }
     private static void search() {
         while (true) {
             try {
@@ -178,16 +182,19 @@ public class ViewMain {
         System.out.println("검색어를 입력해주세요 : ");
         System.out.println("서적 리스트1");
         System.out.println("> {1번째 책 제목}");
+        System.out.println("> {1번째 책 저자}");
         System.out.println("> {북타입: Ebook}");
         System.out.println("> {판매가: 18000원} ");
 
         System.out.println("서적 리스트2");
         System.out.println("> {2번째 책 제목}");
+        System.out.println("> {2번째 책 저자}");
         System.out.println("> {북타입: book}");
         System.out.println("> {판매가: 26000원} ");
         
         System.out.println("서적 리스트3");
         System.out.println("> {3번째 책 제목}");
+        System.out.println("> {3번째 책 저자}");
         System.out.println("> {북타입: book}");
         System.out.println("> {판매가: 21000원} ");
         
@@ -196,23 +203,65 @@ public class ViewMain {
         System.out.println("> {북타입: book}");
         System.out.println("> {판매가: 12000원} ");
         
-        System.out.println("정렬을 선택해주세요 : ");
-        System.out.println("1 베스트셀러 순위  2 리뷰 순위 3 구매 순위 4 선택 99 종료");
-        System.out.println("* 숫자 8을 입력하면 모든 화면에서 장바구니 보기 화면으로 넘어갑니다");
+        // System.out.println("정렬을 선택해주세요 : ");
+        // System.out.println("1 베스트셀러 순위  2 리뷰 순위 3 구매 순위 4 선택 99 종료");
+        // System.out.println("* 숫자 8을 입력하면 모든 화면에서 장바구니 보기 화면으로 넘어갑니다");
 
+        // System.out.println("원하시는 번호를 선택하세요 : ");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("[액션을 선택해주세요[v]]");
+        System.out.println("|-1 원하는 도서 선택");
+        System.out.println("|-2 도서 재검색");
+        System.out.println("|-3 재정렬");
+        allPageOption(); // 나머지 옵션
+        int num = scanner.nextInt();
+        
+        switch (num) {
+            case 1: // 원하는 도서 선택
+                detail();
+                
+                break;
+            case 2: // search 인데 무한루프가 되지 않을까? 검토 필요.
+                search();
+                break;
+            case 3: // 재정렬
+                sortAgain();
+                break;
+            case 0:
+                home();
+                break;
+            case 99:
+                exit();
+                break; 
+            default:
+                exit();
+                break;
+        }
     }
     private static void detail() {
         while (true) {
             try {
+
                 System.out.println("> 책 상세 정보");
+                System.out.println("=====================");
                 System.out.println("- 서적명:");
                 System.out.println("- 저자:");
+                System.out.println("- 출판사:");
+                System.out.println("- 출판일:");
+                System.out.println("=====================");
+                System.out.println();
+
                 System.out.println("> 중고 서적 가격표");
-                System.out.println("- 최상 : ");
-                System.out.println("- 상 : ");
-                System.out.println("- 중 : ");
+                System.out.println("=====================");
+                System.out.println("- 최상" + "{개수}" + " : " + "{가격}");
+                System.out.println("- 상"   + "{개수}" + " : " + "{가격}");
+                System.out.println("- 중"   + "{개수}" + " : " + "{가격}");
+                System.out.println("- 하"   + "{개수}" + " : " + "{가격}");
+                System.out.println("=====================");
+
                 System.out.println("원하시는 번호를 선택하세요 : ");
-                System.out.println("1 장바구니 담기  2 해당 서적 바로 구매 3 상위 메뉴 이동 99 종료");
+                System.out.println("1 장바구니 담기  2 해당 서적 바로 구매 0.홈페이지  99.  종료");
                 Scanner scanner = new Scanner(System.in);
                 int num = scanner.nextInt();
 
@@ -259,6 +308,8 @@ public class ViewMain {
                 int num = scanner.nextInt();
 
                 switch (num) {
+                    case 0:
+
                     case 1:
                         System.out.println("주문 중입니다");
                         System.out.println("주문이 완료되었습니다");
@@ -273,14 +324,28 @@ public class ViewMain {
             }
         }
     }
+    private static void allPageOption() { // return 값을 주는게 좋아보이는데
+        System.out.println("====================[그외]====================");
+        System.out.println("> 0. 홈페이지");
+        System.out.println("= 99. 종료     ");
+        System.out.println("====================");        
+        
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("홈페이지로 이동합니다.");
+                break;
+            case 2:
+                System.out.println("종료합니다.");
+                break;
+            default:
+                System.out.println("잘못된 입력입니다.");
+                break;
+        }
+
+    }
 }
 
 
-private static void allPageOption() { // return 값을 주는게 좋아보이는데
-    System.out.println("> {3번째 책 제목}");
-    System.out.println("> {북타입: book}");
-    System.out.println("> {판매가: 21000원} ");
-    System.out.println("> {3번째 책 제목}");
-    System.out.println("> {북타입: book}");
-    System.out.println("> {판매가: 21000원} ");
-}
