a5cfa3b18e0ca748b26946de3d6aa376434c63ea
eycho93 <eycho93.eyc@gmail.com>
2025-04-10 19:16:18 +0900

    SSSS

--- src/domain/dao/BookDao.java
+++ src/domain/dao/BookDao.java
@@ -1,4 +1,44 @@
 package domain.dao;
 
-public class BookDao extends ParentDao{
+// public class BookDao extends ParentDao{
+
+// }
+
+
+import domain.dto.book.BookListItemResponseDto;
+import domain.dto.book.BookInfoDetailResponseDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDao extends ParentDao {
+
+    public List<BookListItemResponseDto> findAllByKeyword(String keyword) {
+        // 실제 DB 연동 전에 임시 데이터 반환
+        List<BookListItemResponseDto> list = new ArrayList<>();
+        list.add(new BookListItemResponseDto("자바의 정석", "남궁성"));
+        list.add(new BookListItemResponseDto("Effective Java", "조슈아 블로흐"));
+        return list;
+    }
+
+    public List<BookListItemResponseDto> findBestByKeyword(String keyword) {
+        List<BookListItemResponseDto> list = new ArrayList<>();
+        list.add(new BookListItemResponseDto("이것이 자바다", "신용권"));
+        return list;
+    }
+
+    public List<BookListItemResponseDto> findNewByKeyword(String keyword) {
+        List<BookListItemResponseDto> list = new ArrayList<>();
+        list.add(new BookListItemResponseDto("모던 자바 인 액션", "라울-게이브리얼 우르마"));
+        return list;
+    }
+
+    public BookInfoDetailResponseDto findById(int bookId) {
+        return BookInfoDetailResponseDto.builder()
+                .title("자바 프로그래밍")
+                .author("홍길동")
+                .publisher("코딩출판사")
+                .publishedDate("2023-01-01")
+                .build();
+    }
 }
--- src/domain/dto/book/BookListItemResponseDto.java
+++ src/domain/dto/book/BookListItemResponseDto.java
@@ -1,4 +1,26 @@
 package domain.dto.book;
 
 public class BookListItemResponseDto {
+    private String title;
+    private String author;
+
+    public BookListItemResponseDto(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getter
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    // (선택) toString()
+    @Override
+    public String toString() {
+        return "📘 " + title + " | 저자: " + author;
+    }
 }
